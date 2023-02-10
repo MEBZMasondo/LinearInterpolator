f0984d0951096cee26916c1882571b8a2ac25f55
MEBZMasondo <mondlime8@gmail.com>
2023-02-11 00:41:29 +0200

    Initial commit

---
+++ Interpolator/FormInt.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Interpolator
+{
+    partial class FormInt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.varx1TextBox = new System.Windows.Forms.TextBox();
+            this.varx2TextBox = new System.Windows.Forms.TextBox();
+            this.varx3TextBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.vary3TextBox = new System.Windows.Forms.TextBox();
+            this.vary2TextBox = new System.Windows.Forms.TextBox();
+            this.vary1TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // varx1TextBox
+            // 
+            this.varx1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.varx1TextBox.Location = new System.Drawing.Point(12, 68);
+            this.varx1TextBox.Name = "varx1TextBox";
+            this.varx1TextBox.Size = new System.Drawing.Size(122, 31);
+            this.varx1TextBox.TabIndex = 0;
+            // 
+            // varx2TextBox
+            // 
+            this.varx2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.varx2TextBox.Location = new System.Drawing.Point(12, 107);
+            this.varx2TextBox.Name = "varx2TextBox";
+            this.varx2TextBox.Size = new System.Drawing.Size(122, 31);
+            this.varx2TextBox.TabIndex = 1;
+            // 
+            // varx3TextBox
+            // 
+            this.varx3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.varx3TextBox.Location = new System.Drawing.Point(12, 144);
+            this.varx3TextBox.Name = "varx3TextBox";
+            this.varx3TextBox.Size = new System.Drawing.Size(122, 31);
+            this.varx3TextBox.TabIndex = 2;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.Location = new System.Drawing.Point(12, 211);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(122, 34);
+            this.calcButton.TabIndex = 6;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(145, 211);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(122, 34);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // vary3TextBox
+            // 
+            this.vary3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vary3TextBox.Location = new System.Drawing.Point(145, 144);
+            this.vary3TextBox.Name = "vary3TextBox";
+            this.vary3TextBox.Size = new System.Drawing.Size(122, 31);
+            this.vary3TextBox.TabIndex = 5;
+            // 
+            // vary2TextBox
+            // 
+            this.vary2TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.vary2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vary2TextBox.Location = new System.Drawing.Point(145, 107);
+            this.vary2TextBox.Name = "vary2TextBox";
+            this.vary2TextBox.ReadOnly = true;
+            this.vary2TextBox.Size = new System.Drawing.Size(122, 31);
+            this.vary2TextBox.TabIndex = 4;
+            // 
+            // vary1TextBox
+            // 
+            this.vary1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vary1TextBox.Location = new System.Drawing.Point(145, 68);
+            this.vary1TextBox.Name = "vary1TextBox";
+            this.vary1TextBox.Size = new System.Drawing.Size(122, 31);
+            this.vary1TextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(192, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.actionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(655, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // historyListBox
+            // 
+            this.historyListBox.FormattingEnabled = true;
+            this.historyListBox.Location = new System.Drawing.Point(293, 72);
+            this.historyListBox.Name = "historyListBox";
+            this.historyListBox.Size = new System.Drawing.Size(350, 173);
+            this.historyListBox.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "EVALUATION HISTORY";
+            // 
+            // FormInt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 256);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.historyListBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.vary3TextBox);
+            this.Controls.Add(this.vary2TextBox);
+            this.Controls.Add(this.vary1TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.varx3TextBox);
+            this.Controls.Add(this.varx2TextBox);
+            this.Controls.Add(this.varx1TextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormInt";
+            this.Text = "LINEAR INTERPOLATOR";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox varx1TextBox;
+        private System.Windows.Forms.TextBox varx2TextBox;
+        private System.Windows.Forms.TextBox varx3TextBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox vary3TextBox;
+        private System.Windows.Forms.TextBox vary2TextBox;
+        private System.Windows.Forms.TextBox vary1TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ListBox historyListBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Interpolator/FormInt.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interpolator
+{
+    public partial class FormInt : Form
+    {
+        public FormInt()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // calc
+                double varx1Double = Double.Parse(varx1TextBox.Text);
+                double varx2Double = Double.Parse(varx2TextBox.Text);
+                double varx3Double = Double.Parse(varx3TextBox.Text);
+
+                double vary1Double = Double.Parse(vary1TextBox.Text);
+                //double vary2Double = Double.Parse(vary2TextBox.Text);
+                double vary3Double = Double.Parse(vary3TextBox.Text);
+
+                // RESULT FOR y2
+                double AnsDouble = -(((vary3Double - vary1Double) * (varx3Double - varx2Double) / (varx3Double - varx1Double)) - vary3Double);
+                // RESULT FOR x2
+                //AnsDouble = -(((vary3Double - vary2Double) * (varx3Double - varx1Double) / (vary3Double - vary1Double)) - varx3Double)
+                vary2TextBox.Text = AnsDouble.ToString();
+
+                String histString = "x1 = " + varx1Double   + "\ty1 = "   + vary1Double + 
+                                    "\tx2 = " + varx2Double   + "\ty2 = [ "   + AnsDouble +  " ]" +
+                                    "\tx3 = " + varx3Double   + "\ty3 = "   + vary3Double ; 
+                historyListBox.Items.Add( histString);
+                clearEntries();
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            clearEntries();
+        }
+
+        public void clearEntries()
+        {
+            // clear
+            varx1TextBox.Clear();
+            varx2TextBox.Clear();
+            varx3TextBox.Clear();
+
+            vary1TextBox.Clear();
+            vary2TextBox.Clear();
+            vary3TextBox.Clear();
+
+            varx1TextBox.Focus();
+        }
+    }
+}
